8bffe241993b52f7730dcbcf426f1627f9985499
smithwebtek <brad@smithwebtek.com>
2018-04-19 20:10:50 -0400

    Update api url

--- src/store/services/TaskService.js
+++ src/store/services/TaskService.js
@@ -1,5 +1,5 @@
 // const API_URL = process.env.REACT_APP_API_URL || "https://swt-jobsearch-api.herokuapp.com/api"\
-const API_URL = "https://swt-jobsearch-api.herokuapp.com/api" || process.env.REACT_APP_API_URL
+const API_URL = "https://swt-jobsearch-api.herokuapp.com/" || process.env.REACT_APP_API_URL
 
 const TaskService = {
 	createTask(task) {
