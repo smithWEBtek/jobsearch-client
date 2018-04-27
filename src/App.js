060bba303b67141af2bd0eeb8560d28ffd24a275
smithwebtek <brad@smithwebtek.com>
2018-04-27 18:07:54 -0400

    Company create edit and routes begun

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Route } from 'react-router-dom'
+// import Aux from '../src/HOC/Aux/Aux'
 
 import Companies from '../src/Companies/Companies'
 import Contacts from '../src/Contacts/Contacts'
@@ -30,41 +33,48 @@ class App extends Component {
 					<div className='col'>
 						<h4 className='DataBackGround'>Companies</h4>
 						<Companies />
+						<Route path="/companies" component={Companies} />
 					</div>
 
 					<div className='col'>
 						<h4 className='DataBackGround'>Contacts</h4>
 						<Contacts />
+						<Route path="/contacts" component={Contacts} />
 					</div>
 					<br />
 
 					<div className='col'>
 						<h4 className='DataBackGround'>Jobs</h4>
 						<Jobs />
+						<Route path="/jobs" component={Jobs} />
 					</div>
 					<br />
 
 					<div className='col'>
 						<h4 className='DataBackGround'>Users</h4>
 						<Users />
+						<Route path="/users" component={Users} />
 					</div>
 					<br />
 
 					<div className='col'>
 						<h4 className='DataBackGround'>Steps</h4>
 						<Steps />
+						<Route path="/steps" component={Steps} />
 					</div>
 					<br />
 
 					<div className='col'>
 						<h4 className='DataBackGround'>Tasks</h4>
 						<Tasks />
+						<Route path="/tasks" component={Tasks} />
 					</div>
 					<br />
 
 					<div className='col'>
 						<h4 className='DataBackGround'>Logs</h4>
 						<Logs />
+						<Route path="/logs" component={Logs} />
 					</div>
 					<br />
 
