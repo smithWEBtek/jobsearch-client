53e4dac55370bdbfb49d060dd7bde57cec99f72e
smithwebtek <brad@smithwebtek.com>
2018-04-30 13:26:41 -0400

    Update EditCompany

--- src/Companies/EditCompany/EditCompany.js
+++ src/Companies/EditCompany/EditCompany.js
@@ -13,89 +13,88 @@ class EditCompany extends Component {
 	}
 
 	componentDidMount() {
-		this.setState({ company:  })
+		this.setState({ company: this.props.company })
 	}
-}
 
-handleChange = (e) => {
-	const { name, value } = e.target;
-	this.setState({ [name]: value });
-	e.preventDefault()
-}
+	handleChange = (e) => {
+		const { name, value } = e.target;
+		this.setState({ [name]: value });
+		e.preventDefault()
+	}
 
-handleCancel = () => {
-	this.props.close()
-}
+	handleCancel = () => {
+		this.props.close()
+	}
 
-handleSubmit = (e) => {
-	let data = this.state.company
-	let history = this.props.history
-	this.props.update(data, history)
-}
+	handleSubmit = (e) => {
+		let data = this.state.company
+		let history = this.props.history
+		this.props.update(data, history)
+	}
 
-render() {
+	render() {
 
-	return (
-		<div>
-			<p className="FormInstructions">Edit form and click 'Update Company'</p>
-			<form className="Form">
-				<p><label htmlFor="name">First name </label>
-					<input
-						type="text"
-						name="name"
-						value={this.state.name}
-						onChange={this.handleChange}
-					/></p>
-				<p><label>Name</label>
-					<input
-						type="text"
-						name="name"
-						value={this.state.name}
-						onChange={this.handleChange}
-					/></p>
-				<p><label>City </label>
-					<input
-						type="text"
-						name="city"
-						value={this.state.city}
-						onChange={this.handleChange}
-					/></p>
-				<p><label>State </label>
-					<input
-						type="text"
-						name="state"
-						value={this.state.state}
-						onChange={this.handleChange}
-					/></p>
-				<p><label>Url</label>
-					<input
-						type="text"
-						name="url"
-						value={this.state.url}
-						onChange={this.handleChange}
-					/></p>
-				<p><label>About</label>
-					<input
-						type="text"
-						name="about"
-						value={this.state.about}
-						onChange={this.handleChange}
-					/></p>
-				<button
-					type="button"
-					name="cancel"
-					onClick={this.handleCancel}
-					className="Danger"
-				>CANCEL</button>
-				<button
-					type='button'
-					className="Success"
-					onClick={(e) => this.handleSubmit(e)}
-				>SAVE</button>
-			</form>
-		</div>
-	)
-}
+		return (
+			<div>
+				<p className="FormInstructions">Edit form and click 'Update Company'</p>
+				<form className="Form">
+					<p><label htmlFor="name">First name </label>
+						<input
+							type="text"
+							name="name"
+							value={this.state.name}
+							onChange={this.handleChange}
+						/></p>
+					<p><label>Name</label>
+						<input
+							type="text"
+							name="name"
+							value={this.state.name}
+							onChange={this.handleChange}
+						/></p>
+					<p><label>City </label>
+						<input
+							type="text"
+							name="city"
+							value={this.state.city}
+							onChange={this.handleChange}
+						/></p>
+					<p><label>State </label>
+						<input
+							type="text"
+							name="state"
+							value={this.state.state}
+							onChange={this.handleChange}
+						/></p>
+					<p><label>Url</label>
+						<input
+							type="text"
+							name="url"
+							value={this.state.url}
+							onChange={this.handleChange}
+						/></p>
+					<p><label>About</label>
+						<input
+							type="text"
+							name="about"
+							value={this.state.about}
+							onChange={this.handleChange}
+						/></p>
+					<button
+						type="button"
+						name="cancel"
+						onClick={this.handleCancel}
+						className="Danger"
+					>CANCEL</button>
+					<button
+						type='button'
+						className="Success"
+						onClick={(e) => this.handleSubmit(e)}
+					>SAVE</button>
+				</form>
+			</div>
+		)
+	}
 }
 
 export default EditCompany
